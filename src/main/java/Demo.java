9125c71f9a5bea5cc0f89356dd026d09c9b90b04
VladimirVornikov <vornikov-vladimir@mail.ru>
2023-07-12 11:41:03 +0200

    12/07

---
+++ src/main/java/Demo.java
@@ -0,0 +1,10 @@
+//Let's code for real!
+//        The given program outputs “Coding is boring”.
+//        But that’s not true, is it? Let’s change the message.
+//        Task:
+//          Modify the code to output "Coding is fun" to the screen.
+class Demo{
+    public static void main(String[] args) {
+        System.out.println("Coding is fun");
+    }
+}
